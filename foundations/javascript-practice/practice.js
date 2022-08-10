d97e71480d508b01e1cade21917540a0c27c1a95
djcartwrightweb <dj.cartwright.web@gmail.com>
2022-08-10 16:37:32 -0400

    javascript practice

---
+++ foundations/javascript-practice/practice.js
@@ -0,0 +1,21 @@
+console.log("test");
+
+console.log(23 + 97);
+
+console.log(34 + 34 + 34 + 34 + 34 + 34);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+console.log(9*a);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log(percentage);
